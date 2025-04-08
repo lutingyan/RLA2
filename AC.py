6dbde81f94e4fbb45f9549f4a0f22e42eb675003
Jiaxuan Yu <166388707+WZHDXXT@users.noreply.github.com>
2025-04-08 20:33:03 +0200

    Add files via upload

--- AC.py
+++ AC.py
@@ -24,11 +24,13 @@ class Actor(nn.Module):
     def __init__(self, state_dim, action_dim, hidden_dim):
         super().__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        return F.softmax(self.fc2(x), dim=-1)
+        x = F.relu(self.fc2(x))
+        return F.softmax(self.fc3(x), dim=-1)
 
     def get_action(self, state):
         state = torch.FloatTensor(state)
@@ -42,11 +44,13 @@ class Critic(nn.Module):
     def __init__(self, state_dim, hidden_dim):
         super().__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, 1)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        return self.fc2(x)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
 
 # Actor-Critic 主函数
 def train_actor_critic(seed=0):
